09b3cf51892c9617965b671dd47217eafe03ef5a
Patryk Orłowski <orlowski.dev@gmail.com>
2023-06-14 16:57:22 +0200

    01 - Initail commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+	return <div className="main-container">
+		<h1>Hello world</h1>
+
+		<div className="attribution">
+			Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+			Coded by <a href="https://www.frontendmentor.io/profile/orlowski-dev">@orlowski-dev</a>.
+  		</div>
+	</div>
+}
