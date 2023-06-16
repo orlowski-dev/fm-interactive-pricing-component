b66c949904d57c66b072bb31dadc7da71537d8bd
Patryk Orłowski <orlowski.dev@gmail.com>
2023-06-16 14:33:56 +0200

    05 - Now component is able to calculating the price

--- src/PricingComponent.jsx
+++ src/PricingComponent.jsx
@@ -1,22 +1,55 @@
+import { useState } from 'react';
 import './PricingComponent.css';
 
 export default function PricingComponent() {
+
+    const rangeInputAttrs = {
+        min: 1.00,
+        max: 200.00,
+    }
+    const [countYearly, setCountYearly] = useState(false);
+    const [rangeInputValue, setRangeInputValue] = useState(rangeInputAttrs.max / 2);
+
+    const PriceLabel = () => {
+        return countYearly ? <p>/ year</p> : <p>/ month</p>
+    }
+    
+    const calculatePrice = () => {
+        if (countYearly) {
+            const priceWithoutDiscount = rangeInputValue * 0.16 * 12;
+            const discount = priceWithoutDiscount * .25;
+            return Number(priceWithoutDiscount - discount).toFixed(2);
+        }
+        
+        const priceWithoutDiscount = rangeInputValue * 0.16;
+        return priceWithoutDiscount.toFixed(2);
+    }
+
     return <article className="pricing-component">
         <section className="top-section">
             <div className="pageviews-container">
                 <h2>100K Pageviews</h2>
             </div>
             <div className="slider-container">
-                <input type="range" id="pricing-slider"/>
+                <input type="range" id="pricing-slider"
+                    min={rangeInputAttrs.min}
+                    max={rangeInputAttrs.max}
+                    defaultValue={rangeInputAttrs.max / 2}
+                    onChange={e => setRangeInputValue(e.target.value)}
+                />
             </div>
             <div className="price-container">
-                <h2 id='price-amount' className='price-amount'>$16.00</h2>
-                <p>/ month</p>
+                <h2 id='price-amount' className='price-amount'>${
+                    calculatePrice()
+                }</h2>
+                <PriceLabel />
             </div>
             <div className="billing-container">
                 <p>Monthly Billing</p>
                 <div className='custom-checkbox'>
-                    <input type="checkbox" id="billing-yearly-checkbox"/>
+                    <input type="checkbox" id="billing-yearly-checkbox" onChange={()=>{
+                        setCountYearly((countYearly ? false : true));
+                    }}/>
                     <label htmlFor="billing-yearly-checkbox"></label>
                 </div>
                 <p className='discount-p'>
